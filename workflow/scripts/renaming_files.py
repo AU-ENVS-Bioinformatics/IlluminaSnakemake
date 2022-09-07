a67a6a00ef0c8f1ea49dda8b6c7a8fe635239f7d
currocam <campuzanocurro@gmail.com>
2022-09-07 12:00:30 +0000

    refactor: better renaming

--- workflow/scripts/renaming_files.py
+++
@@ -1,5 +0,0 @@
-import os
-import shutil
-for original, target in zip(snakemake.input, snakemake.output):
-    os.makedirs(os.path.dirname(target), exist_ok=True)
-    shutil.copyfile(original, target)
