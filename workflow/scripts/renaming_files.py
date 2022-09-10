7ed8221a81723644eba07638e6086cbf862aa357
currocam <campuzanocurro@gmail.com>
2022-09-10 09:39:05 +0000

    fixed: renaming files more flexible

---
+++ workflow/scripts/renaming_files.py
@@ -0,0 +1,21 @@
+import os
+import shutil
+import re
+
+regular_expression = ".+-(.+)_.+_.+_(.+)_.+\.fastq.gz"
+input_dir = snakemake.params[0]
+output_dir = snakemake.params[1]
+
+def copy_file(original: str, target: str) -> None:
+    os.makedirs(os.path.dirname(target), exist_ok=True)
+    shutil.copyfile(original, target)
+
+for file in os.listdir(input_dir):
+    match = re.match(regular_expression, file)
+    if match:
+        original = input_dir + match.string
+        print(original)
+        target = output_dir + match.group(1) + "_" + match.group(2) + ".fastq.gz"
+        print(target)
+        copy_file (original, target)
+  
